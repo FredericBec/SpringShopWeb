f905c2494ad05c7d9793153684483fa3b09524c1
Frederic Bec <bec.frederic90@outlook.fr>
2024-04-10 10:15:32 +0200

    Add mapping article and save

--- src/main/java/fr/fms/web/ArticleController.java
+++ src/main/java/fr/fms/web/ArticleController.java
@@ -2,16 +2,20 @@ package fr.fms.web;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 import fr.fms.dao.ArticleRepository;
 import fr.fms.entities.Article;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -21,6 +25,17 @@ public class ArticleController {
 	@Autowired
 	ArticleRepository articleRepository;
 	
+	/*
+	@GetMapping("/index")
+	public String index(Model model, @RequestParam(name="page", defaultValue = "0") int page) {
+		Page<Article> articles = articleRepository.findAll(PageRequest.of(page, 5));
+		model.addAttribute("listArticle", articles);
+		model.addAttribute("pages", new int[articles.getTotalPages()]);
+		model.addAttribute("currentPage", page);
+		return "articles";
+	}
+	*/
+	
 	@GetMapping("/index")
 	public String index(Model model, @RequestParam(name="page", defaultValue = "0") int page, @RequestParam(name = "keyword", defaultValue = "") String kw) {
 		Page<Article> articles = articleRepository.findByDescriptionContains(kw, PageRequest.of(page, 5));
@@ -29,6 +44,21 @@ public class ArticleController {
 		model.addAttribute("pages", new int[articles.getTotalPages()]);
 		model.addAttribute("currentPage", page);
 		return "articles";
+	}
+	
+	@GetMapping("/article")
+	public String article(Model model) {
+		model.addAttribute("article", new Article());
+		return "article";
+	}
+	
+	@PostMapping("/save")
+	public String save(Model model, @Valid Article article, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) return "article";
+		
+		articleRepository.save(article);
+		return "redirect:/index";
 	}
 	
 	@GetMapping("/delete")
